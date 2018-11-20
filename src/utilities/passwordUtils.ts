7dcffe376bf78745f077def9a5efbec37701bce5
Akshay Goud Manchala <akshaym@VALUEMOMENTUM.BIZ>
2018-11-20 15:36:19 +0530

    password commit

--- src/utilities/passwordUtils.ts
+++ src/utilities/passwordUtils.ts
@@ -43,14 +43,30 @@ export default class ScriptUtilities {
     }
 
     public static CheckPasswordStrength(value: string, options: object): PasswordStrength {
-        // let weekOptions: any = {
-        //     minAlphabets: 0,
-        //     minNumerics: 0,
-        //     minSpecialChars: 0,
-        //     maxAlphabets: 0,
-        //     maxNumberics: 0,
-        //     maxSpecialChars: 0
-        // }
+        let weekOptions: any = {
+            minAlphabets: 6,
+            minNumerics: 2,
+            minSpecialChars: 0,
+            maxAlphabets: 20,
+            maxNumberics: 6,
+            maxSpecialChars: 0
+        }
+          let mediumOptions: any = {
+            minAlphabets: 10,
+            minNumerics: 4,
+            minSpecialChars: 0,
+            maxAlphabets: 25,
+            maxNumberics: 12,
+            maxSpecialChars: 0
+        }
+          let strongOptions: any = {
+            minAlphabets: 30,
+            minNumerics: 10,
+            minSpecialChars: 10,
+            maxAlphabets: 40,
+            maxNumberics: 15,
+            maxSpecialChars: 17
+        }
         return PasswordStrength.Week;
     }
 }
